596bfee379b16b18bf375cb4d7b8bdfc1dfc5da8
Victoria <a01734739@tec.mx>
2026-06-05 09:52:50 -0600

    Act 11

---
+++ src/db/migrations/1779380844242_add-priority-to-requests.js
@@ -0,0 +1,24 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.addColumn('requests', {
+    priority: { type: 'varchar(20)', notNull: true, default: 'normal' }
+  })
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.dropColumn('requests', 'priority')
+};
